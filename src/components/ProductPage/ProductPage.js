f02db10bae70dabd781f2d720fa617165a25a079
woohoo <woohoo1607@gmail.com>
2020-03-02 23:08:42 +0200

    mini commit

--- src/components/ProductPage/ProductPage.js
+++ src/components/ProductPage/ProductPage.js
@@ -26,31 +26,36 @@ const ProductPage = (props) => {
                         <p>{props.products.product.price} <span>грн.</span></p>
                         <button className="add-to-cart-btn">Купить</button>
                     </div>
-                    <div className="productDelivery">
-                        <h3>Доставка:</h3>
-                        <ul>
-                            <li>Самовывоз из магазина</li>
-                            <li>Доставка в отделение "Нова пошта"</li>
-                            <li>Адрессная доставка "Нова пошта"</li>
-                        </ul>
+                    <div>
+                        
                     </div>
-                    <div className="productGuarantee">
-                        <h3>Гарантия:</h3>
-                        <ul>
-                            <li>Гарантия {props.products.product.guarantee ? props.products.product.guarantee : 12} месяцев</li>
-                            <li>Обмен/возврат в течении 14 дней</li>
-                        </ul>
-                    </div>
-                    <div className="productPayment">
-                        <h3>Оплата:</h3>
-                        <ul>
-                            <li>Наличными при получении</li>
-                            <li>Картой на сайте</li>
-                            <li>Картой в магазине</li>
-                            <li>Наложенный платеж</li>
-                            <li>Рассрочка онлайн</li>
-                            <li>Перевод с VISA/MasterCard</li>
-                        </ul>
+                    <div>
+                        <div className="productDelivery">
+                            <h3>Доставка:</h3>
+                            <ul>
+                                <li>Самовывоз из магазина</li>
+                                <li>Доставка в отделение "Нова пошта"</li>
+                                <li>Адрессная доставка "Нова пошта"</li>
+                            </ul>
+                        </div>
+                        <div className="productGuarantee">
+                            <h3>Гарантия:</h3>
+                            <ul>
+                                <li>Гарантия {props.products.product.guarantee ? props.products.product.guarantee : 12} месяцев</li>
+                                <li>Обмен/возврат в течении 14 дней</li>
+                            </ul>
+                        </div>
+                        <div className="productPayment">
+                            <h3>Оплата:</h3>
+                            <ul>
+                                <li>Наличными при получении</li>
+                                <li>Картой на сайте</li>
+                                <li>Картой в магазине</li>
+                                <li>Наложенный платеж</li>
+                                <li>Рассрочка онлайн</li>
+                                <li>Перевод с VISA/MasterCard</li>
+                            </ul>
+                        </div>
                     </div>
                 </div>
             </div>

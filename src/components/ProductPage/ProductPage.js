0b32b421a410441ef396c52e88e25d4dcd103a9d
Makc <woohoo1607@gmail.com>
2020-03-03 17:55:40 +0200

    feat: added mainCharacteristics and product menu in productPage

--- src/components/ProductPage/ProductPage.js
+++ src/components/ProductPage/ProductPage.js
@@ -5,9 +5,15 @@ import noProductImg from "../../img/product-no-image.jpg";
 const ProductPage = (props) => {
     console.log(props.products.product);
     let photos = [];
+    let mainCharacteristics = [];
     if (props.products.product.attributes!==undefined) {
         photos = props.products.product.attributes.filter(a => {
             if (a.name === "Фото") {
+                return a
+            }
+        });
+        mainCharacteristics = props.products.product.attributes.filter(a => {
+            if (a.name === "Диагональ дисплея" || a.name === "Разрешение дисплея" || a.name === "Оперативная память" || a.name === "Основная камера" || a.name === "Класс защиты") {
                 return a
             }
         });
@@ -15,6 +21,14 @@ const ProductPage = (props) => {
     return (
         <div className="center">
             <div className="productInfo">
+                <div className="productMenu">
+                    <ul>
+                        <li>Главное</li>
+                        <li>Характеристики</li>
+                        <li>Описание</li>
+                    </ul>
+                    <div className="clr"></div>
+                </div>
                 <div className="productInfoPictures">
                     {photos.length>0 && <img src={photos[0].value} />}
                     {photos.length===0 && <img src={noProductImg} />}
@@ -26,10 +40,34 @@ const ProductPage = (props) => {
                         <p>{props.products.product.price} <span>грн.</span></p>
                         <button className="add-to-cart-btn">Купить</button>
                     </div>
-                    <div>
-                        
+                    <div className="productInfoLeft">
+                        <h3>Краткие технические характеристики:</h3>
+                        {mainCharacteristics.map(a=> {
+                            return (
+                                <p>
+                                    <span className="spanAttributesName">{a.name}: </span>
+                                    <span>{a.value}</span>
+                                </p>
+                            )
+                        })}
+                        <div className="moreCharacteristics"></div>
+{/*                        <h3>Характеристики:</h3>
+                        <table id="product-attributes">
+                            <colgroup>
+                                <col width="35%" />
+                                <col />
+                            </colgroup>
+                            {props.products.product.attributes && props.products.product.attributes.map(a => {
+                                return (
+                                    <tr>
+                                        <th className="tableLable">{a.name}</th>
+                                        <td className="tableData">{a.value}</td>
+                                    </tr>
+                                )
+                            })}
+                        </table>*/}
                     </div>
-                    <div>
+                    <div className="productInfoRight">
                         <div className="productDelivery">
                             <h3>Доставка:</h3>
                             <ul>

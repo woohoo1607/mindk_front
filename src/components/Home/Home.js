72edcc3cdeae459ed044aaaa83f71967255c1853
Makc <woohoo1607@gmail.com>
2020-03-11 17:15:46 +0200

    feat: added paginator

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -11,12 +11,26 @@ const Home = (props) => {
             <section className="productsContainer">
                 {props.products.map(p=> <NavLink to={`/products/${p.id}`} key={p.id}><ProductCard key={p.id} product={p}/></NavLink>)}
             </section>
-            <ReactPaginate pageCount={totalPages}
+            <ReactPaginate pageCount={20}
                            pageRangeDisplayed={3}
                            marginPagesDisplayed={3}
                            initialPage={props.currentPage-1}
                            onPageChange={(e)=>props.setCurrentPage(e.selected+1)}
+                           pageClassName={"paginatorLi"}
+                           pageLinkClassName={"paginatorA"}
+                           activeClassName={"activePaginatorLi"}
+                           previousClassName={"prevPaginatorLi"}
+                           nextClassName={"nextPaginatorLi"}
+                           previousLinkClassName={"prevPaginatorA"}
+                           nextLinkClassName={"nextPaginatorA"}
+                           containerClassName={"paginator"}
+                           previousLabel={''}
+                           nextLabel={''}
+                           activeLinkClassName={"activePaginatorA"}
+                           breakClassName={"paginatorBreakLi"}
+                           breakLinkClassName={"paginatorBreakA"}
             />
+
         </div>
     )
 };

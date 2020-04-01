cb035f9890150d362be040b15962d0ea230a5c09
Makc <woohoo1607@gmail.com>
2020-04-01 13:32:17 +0300

    fix: CategoriesMenu refactored

---
+++ src/components/Home/CategoriesMenu/CategoriesMenu.js
@@ -0,0 +1,26 @@
+import React, {useState} from "react";
+import List from "@material-ui/core/List";
+
+import useStyles from "./CategoriesMenuStyles";
+import CreateMenuLi from "./CreateMenuLi";
+
+const CategoriesMenu = (props) => {
+    const classes = useStyles();
+    const CLOSE = 999999;
+    const [whoOpen, setWhoOpen] = useState(CLOSE);
+
+    const changeWhoOpen = (id) => {
+        setWhoOpen(id);
+    };
+
+
+    return (
+        <div className={classes.categoriesMenu} >
+            <List component="nav" className={classes.menuNav} onMouseLeave={()=> changeWhoOpen(CLOSE)}>
+                {props.categories.map((c, i)=> <CreateMenuLi id={c.id} name={c.name} childrenCount={c.children.length} children={c.children} index={i} changeWhoOpen={changeWhoOpen} whoOpen={whoOpen}/>)}
+            </List>
+        </div>
+    )
+};
+
+export default CategoriesMenu;
